5f7efdd292a07a783ba4bd55ece45c2a975b8fc9
anushagowda2000 <anushagowda.xworkz@gmail.com>
2022-09-22 12:26:20 +0530

    commiting variable files

---
+++ Cooler.java
@@ -0,0 +1,12 @@
+class Cooler
+ {
+
+ static String brandName;
+ static String wattage;
+ static String durability;
+ static double price;
+
+
+
+
+}
---
+++ Refrigerator.java
@@ -0,0 +1,13 @@
+class Refrigerator{
+
+  static String brandName;
+  static String doorType;
+  static String shelfType;
+  static String colour;
+  static String capacity;
+  
+  }
+  
+  
+  
+  
