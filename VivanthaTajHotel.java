79143ac439071e91d8cc59cb3d2243fea290c1fd
anushagowda2000 <anushagowda.xworkz@gmail.com>
2022-09-22 13:09:19 +0530

    commiting method files

---
+++ VivanthaTajHotel.java
@@ -0,0 +1,31 @@
+class VivanthaTajHotel{
+  public static void main(String anu[]){
+
+   String type = "5 Star Hotel";
+   String address = "bangalore";
+   String[] managerNames = {"Anusha","Sandesh","Sangeetha","Ganesh","Apoorva"};
+   int noOfManagers = 5;
+   String foodMenu[] = {"roti","biryani","lolipop","chicken curry","pepper chicken","ginger chicken","garlic chicken","machurian","ghee rice","kabab","naan"};
+   System.out.println("Welcome to VivantaTajHotel");
+   System.out.println("VivantaTajHotel is "+ type);
+   System.out.println("VivantaTajHotel address is "+ address);
+   System.out.println("VivantaTajHotel managerNames is ");
+   for(int s=0; s<managerNames.length; s++){
+       System.out.println(managerNames[s] + " ");
+}
+   System.out.println("list of managers " + noOfManagers);
+   for(int a=0; a<foodMenu.length; a++){
+      System.out.println(foodMenu[a] + " ");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+  
