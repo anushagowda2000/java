7b83f5b11d9defe4dcc4a80433ab7a59990f1b25
anushagowda2000 <anushagowda.xworkz@gmail.com>
2022-11-16 23:15:51 +0530

    commiting inheritance files

---
+++ hospital-management/src/com/xworkz/hospitalapp/constant/Gender.java
@@ -0,0 +1,7 @@
+package com.xworkz.hospitalapp.constant;
+
+// enum helps to have constant value in project
+public enum Gender {
+	female, male, transgender, other;
+	// converted to static
+}
---
+++ hospital-management/src/com/xworkz/hospitalapp/hospital/Hospital.java
@@ -0,0 +1,27 @@
+package com.xworkz.hospitalapp.hospital;
+
+import com.xworkz.hospitalapp.hospital.patient.*;
+
+public class Hospital {
+
+	public Patient patient;
+	public boolean isEmergency;
+	public boolean isTreatmentRequired;
+
+	public boolean admit(Patient patient) {
+		boolean isAdmitted = true;
+		if (isTreatmentRequired)
+			if (isEmergency == true) {
+				this.patient = patient;
+				this.patient.displayInfo();
+				System.out.println("Patient can be allowed to ICU ");
+			} else {
+				System.out.println("Add patient to general ward");
+			}
+		else {
+			System.out.println("no treatent required");
+		}
+		return isAdmitted;
+
+	}
+}
